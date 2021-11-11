cbc469cdb7b8b13d6099e4cb8377250f29d48a73
Chrissttp <chrisapjra@gmail.com>
2021-11-10 23:26:02 -0600

    dia1

---
+++ dia1/e1.py
@@ -0,0 +1,25 @@
+#int
+a = 2
+#float
+b = 2.3
+#string
+c = "hello wolrd" 
+#boolean
+d = True
+e = False
+
+
+print (c)
+
+x = 1
+x = x + 1
+print (x)
+ 
+#Type data
+#city = input ("Type the city where you grew up: ")
+
+num1 = int(input ("type your first number:"))
+num2 = int(input("type a second number"))
+sum = num1 + num2
+#print (sum)
+print(f"Sum = {sum}")
---
+++ dia1/e2.py
@@ -0,0 +1,4 @@
+print("wellcome to the bandname generator" )
+city = input("what´s the name of the city you grew up: ")
+pet = input ("what´s your pet´s name:")
+print(f"Your bandname could be: {city} {pet}")
